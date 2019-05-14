dc1c8587ea6223404fce0603b222b561ab3d6dcb
cnotf <956490047@qq.com>
2019-05-14 14:10:47 +0800

    Signed-off-by: cnotf <956490047@qq.com> 商城首页修改

---
+++ src/components/list.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="list">
+    <h1>list.vue</h1>
+    <p>{{msg}}</p>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'list',
+  data () {
+    return {
+      msg: 'Welcome to List.vue'
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import list from '@/components/list'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'hash',
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'list',
+      component: list
     }
   ]
 })
